61762d293f9a245364bd0d6eb42833df8eb7ab94
bhaskar2500 <bhaskar.2500@gmail.com>
2016-12-20 08:55:07 +0530

    creating the commit

---
+++ VendorsAddition/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VendorsAddition.MvcApplication" Language="C#" %>
---
+++ VendorsAddition/Models/VendorModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VendorsAddition.Models
+{
+    public class VendorModel
+    {
+
+       public string ProductName { get; set; }
+        public int ProductQty { get; set; }
+       public string VendorName { get; set; }
+
+    }
+}
